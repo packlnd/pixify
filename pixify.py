a9966e1728a501df66d90f36efe8f63f51974cd4
Patrik Ackland <patrikackland@gmail.com>
2016-02-24 00:04:00 -0800

    Sample example using sklearn kmeans

---
+++ pixify.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
+from sklearn.datasets import load_sample_image
+from sklearn.utils import shuffle
+from time import time
+from scipy.misc import imread
+
+n_colors = 7
+pic = np.array(imread("pic.jpg"), dtype=np.float64) / 255
+w, h, d = original_shape = tuple(pic.shape)
+image_array = np.reshape(pic, (w * h, d))
+image_array_sample = shuffle(image_array, random_state=0)[:1000]
+kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+labels = kmeans.predict(image_array)
+
+def recreate_image(codebook, labels, w, h):
+    """Recreate the (compressed) image from the code book & labels"""
+    d = codebook.shape[1]
+    image = np.zeros((w, h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            image[i][j] = codebook[labels[label_idx]]
+            label_idx += 1
+    return image
+
+plt.figure(1)
+plt.clf()
+ax = plt.axes([0, 0, 1, 1])
+plt.axis('off')
+plt.title('Quantized image (64 colors, K-Means)')
+plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+plt.show()
